bcbd5ea06a24fd08c58a1dca5aca929d0f9ecc3b
mahima97 <mjmodi19@gmail.com>
2020-09-22 17:16:40 +0530

    added randomColor function

--- defaults.py
+++ defaults.py
@@ -15,18 +15,18 @@ BLENDING_LIST = ['none']#BLENDING_LIST = ['gaussian','poisson', 'none', 'box', '
 
 # Parameters for images
 MIN_NO_OF_OBJECTS = 1
-MAX_NO_OF_OBJECTS = 9
+MAX_NO_OF_OBJECTS = 8
 MIN_NO_OF_DISTRACTOR_OBJECTS = 2
 MAX_NO_OF_DISTRACTOR_OBJECTS = 4
-WIDTH = 640
-HEIGHT = 480
-MAX_ATTEMPTS_TO_SYNTHESIZE = 50
+WIDTH = 720
+HEIGHT = 640
+MAX_ATTEMPTS_TO_SYNTHESIZE = 500
 
 # Parameters for objects in images
-MIN_SCALE = 1 # min scale for scale augmentation
-MAX_SCALE = 1 # max scale for scale augmentation
-MAX_DEGREES = 30 # max rotation allowed during rotation augmentation
-MAX_TRUNCATION_FRACTION = 0.25 # max fraction to be truncated = MAX_TRUNCACTION_FRACTION*(WIDTH/HEIGHT)
-MAX_ALLOWED_IOU = 0.2 # IOU > MAX_ALLOWED_IOU is considered an occlusion
+MIN_SCALE = 0.5 # min scale for scale augmentation
+MAX_SCALE = 1.5 # max scale for scale augmentation
+MAX_DEGREES = 270 # max rotation allowed during rotation augmentation
+MAX_TRUNCATION_FRACTION = 0.025 # max fraction to be truncated = MAX_TRUNCACTION_FRACTION*(WIDTH/HEIGHT)
+MAX_ALLOWED_IOU = 0.025 # IOU > MAX_ALLOWED_IOU is considered an occlusion
 MIN_WIDTH = 6 # Minimum width of object to use for data generation
 MIN_HEIGHT = 6 # Minimum height of object to use for data generation
